ef869d406a5c1ff78f4f4f7f658ffdf8f230467a
Ricco Venterea <ricco@nia-login01.scinet.local>
2022-07-20 16:53:50 -0400

    Made running jobs easier

--- movies.py
+++ movies.py
@@ -1,28 +1,42 @@
 from asteroid_script import *
 
-objects = ["bamberga", "ganymed", "pluto", "makemake"]
-astsinfo = ["/home/r/rbond/sigurdkn/project/actpol/ephemerides/objects/Bamberga.npy", "/home/r/rbond/sigurdkn/project/actpol/ephemerides/objects/Ganymed.npy", 
-"/home/r/rbond/sigurdkn/project/actpol/ephemerides/objects/Pluto.npy", "/home/r/rbond/sigurdkn/project/actpol/ephemerides/objects/Makemake.npy"]
+objects = ["erminia", "egeria", "kalliope"]
+astsinfo = "/home/r/rbond/sigurdkn/project/actpol/ephemerides/objects/" 
 arrs = ["pa4", "pa5", "pa6"]
 
 k = 0 
 while k<len(objects):
   #array pa4
-  make_movie(astsinfo[k], objects[k], arrs[0], "f150")
-  make_gallery(objects[k], arrs[0], "f150", directory = "asteroids/" + objects[k] + "/" + arrs[0] + "/f150/")  
-  make_movie(astsinfo[k], objects[k], arrs[0], "f220")
-  make_gallery(objects[k], arrs[0], "f220", directory = "asteroids/" + objects[k] + "/" + arrs[0] + "/f220/")  
+    #f150
+  get_maps(astsinfo + objects[k].capitalize() + ".npy", objects[k], arrs[0], "f150")
+  make_gallery(objects[k], arrs[0], "f150", directory = "test/" + objects[k] + "/" + arrs[0] + "/f150/")  
+  flux_stack(objects[k], arrs[0], "f150", directory = "test/" + objects[k] + "/" + arrs[0] + "/f150/")  
+    
+    #f220
+  get_maps(astsinfo + objects[k].capitalize() + ".npy", objects[k], arrs[0], "f220")
+  make_gallery(objects[k], arrs[0], "f220", directory = "test/" + objects[k] + "/" + arrs[0] + "/f220/")   
+  flux_stack(objects[k], arrs[0], "f220", directory = "test/" + objects[k] + "/" + arrs[0] + "/f220/")   
   
   #array pa5
-  make_movie(astsinfo[k], objects[k], arrs[1], "f090")
-  make_gallery(objects[k], arrs[1], "f090", directory = "asteroids/" + objects[k] + "/" + arrs[1] + "/f090/")  
-  make_movie(astsinfo[k], objects[k], arrs[1], "f150")
-  make_gallery(objects[k], arrs[1], "f150", directory = "asteroids/" + objects[k] + "/" + arrs[1] + "/f150/")  
+    #f090
+  get_maps(astsinfo + objects[k].capitalize() + ".npy", objects[k], arrs[1], "f090")
+  make_gallery(objects[k], arrs[1], "f090", directory = "test/" + objects[k] + "/" + arrs[1] + "/f090/")
+  flux_stack(objects[k], arrs[1], "f090", directory = "test/" + objects[k] + "/" + arrs[1] + "/f090/")  
+    
+    #f150
+  get_maps(astsinfo + objects[k].capitalize() + ".npy", objects[k], arrs[1], "f150")
+  make_gallery(objects[k], arrs[1], "f150", directory = "test/" + objects[k] + "/" + arrs[1] + "/f150/")
+  flux_stack(objects[k], arrs[1], "f150", directory = "test/" + objects[k] + "/" + arrs[1] + "/f150/")    
   
   #array pa6
-  make_movie(astsinfo[k], objects[k], arrs[2], "f090")
-  make_gallery(objects[k], arrs[2], "f090", directory = "asteroids/" + objects[k] + "/" + arrs[2] + "/f090/")  
-  make_movie(astsinfo[k], objects[k], arrs[2], "f150")
-  make_gallery(objects[k], arrs[2], "f150", directory = "asteroids/" objects[k] + "/" + arrs[2] + "/f150/")  
+    #f090
+  get_maps(astsinfo + objects[k].capitalize() + ".npy", objects[k], arrs[2], "f090")
+  make_gallery(objects[k], arrs[2], "f090", directory = "test/" + objects[k] + "/" + arrs[2] + "/f090/")
+  flux_stack(objects[k], arrs[2], "f090", directory = "test/" + objects[k] + "/" + arrs[2] + "/f090/")  
+    
+    #f150
+  get_maps(astsinfo + objects[k].capitalize() + ".npy", objects[k], arrs[2], "f150")
+  make_gallery(objects[k], arrs[2], "f150", directory = "test/" + objects[k] + "/" + arrs[2] + "/f150/")
+  flux_stack(objects[k], arrs[2], "f150", directory = "test/" + objects[k] + "/" + arrs[2] + "/f150/")    
   
   k += 1              
